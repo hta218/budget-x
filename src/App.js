5d3a5b65d263ccaf1e54f48e6a8e6dcb3709effa
Leo Huynh <huynhtuananh21895@gmail.com>
2020-08-30 15:24:28 +0700

    Add TailwindCSS

--- src/App.js
+++ src/App.js
@@ -1,26 +1,21 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import Header from './components/Header'
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+	return <>
+		<Header />
+		<div className="App">
+			<div class="h-64">
+				<div class="p-4 m-4 bg-green-600">
+					<h1 class="text-2xl font-bold text-white">Tailwind CSS Demo</h1>
+				</div>
+				<div class="p-4 m-4 bg-green-300 h-full">
+					<h2 class="text-green-900">Have much fun using Tailwind CSS</h2>
+					<button class="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded">My Tailwind Button</button>
+				</div>
+			</div>
+		</div>
+	</>;
 }
 
 export default App;
---
+++ src/store/app.js
@@ -0,0 +1,7 @@
+import { useState } from 'react'
+import { createContainer } from 'unstated-next'
+
+function useApp() {
+	let [data, setData] = useState({  })
+}
+
